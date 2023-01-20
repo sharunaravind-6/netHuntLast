4900635dc461f6dc4cb6c94c6643253741255905
SanjayBaskaran <sanjayprakashsivakasi@gmail.com>
2023-01-20 15:07:47 +0530

    Custom user models

--- nethuntBackend/user/admin.py
+++ nethuntBackend/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import NethuntUser
+from django.contrib.auth.admin import UserAdmin
+from .forms import (CustomUserChangeForm,CustomUserCreationForm)
 # Register your models here.
+class NethuntUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = NethuntUser
+    list_display = ("email","is_active","is_admin","is_coordinator","is_candidate")
+    list_filter = ("is_admin","is_coordinator","is_candidate")
+admin.site.register(NethuntUser,NethuntUserAdmin)
--- nethuntBackend/user/models.py
+++ nethuntBackend/user/models.py
@@ -1,5 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 # Create your models here.
+class NethuntUserManager(BaseUserManager):
+    def create_user(self,email,password,**extraFields):
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extraFields)
+        user.set_password(password)
+        user.save()
+        return user
 class NethuntUser(AbstractUser):
-    pass
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["is_admin","is_coordinator","is_candidate"]
+    email = models.EmailField( unique=True)
+    objects = NethuntUserManager
+    is_admin = models.BooleanField(default=False)
+    is_candidate = models.BooleanField(default=True)
+    is_coordinator = models.BooleanField(default=False)
