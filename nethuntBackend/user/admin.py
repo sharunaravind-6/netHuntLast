68442655d0a3ad0626da46e341399e9b94642e56
SanjayBaskaran <sanjayprakashsivakasi@gmail.com>
2023-02-09 23:58:27 +0530

    Found the problem that caused in django admin user add page

--- nethuntBackend/user/admin.py
+++ nethuntBackend/user/admin.py
@@ -5,33 +5,48 @@ from .forms import (CustomUserChangeForm,CustomUserCreationForm)
 from .models import (NethuntUser,Candidate,College,Coordinator,NethuntUser)
 # admin.site.register(NethuntUser)
 class NethuntUserAdmin(UserAdmin):
-    list_display = ("email", "is_staff", "is_superuser")
+    list_display = ("email", "role")
     readonly_fields = ("last_login", "date_joined",)
     ordering = ("email",)
-    fieldsets = (
+    add_fieldsets = (
         (
-            "Fields",
+            None,
             {
-                "fields": (
-                    "email",
-                    "uuid",
-                    "date_joined",
-                    "last_login",
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                    "password",
-                )
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
             },
         ),
     )
+    # fieldsets = (
+    #     (
+    #         "Fields",
+    #         {
+    #             "fields": (
+    #                 "email",
+    #                 "uuid",
+    #                 "date_joined",
+    #                 "last_login",
+    #                 "is_active",
+    #                 "is_staff",
+    #                 "is_superuser",
+    #                 "groups",
+    #                 "user_permissions",
+    #                 "password",
+    #             )
+    #         },
+    #     ),
+    # )
     exclude = ["username"]
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = NethuntUser
+class CollegeAdmin(admin.ModelAdmin):
+    model = College
+    # fields = ("collegeName",)
+    exclude = ("collegeCity",)
+    list_display = ("collegeName","collegeCity",)
+    
 admin.site.register(NethuntUser,NethuntUserAdmin)
 admin.site.register(Candidate)
-admin.site.register(College)
+admin.site.register(College,CollegeAdmin)
 admin.site.register(Coordinator)
