9f92a8e403a0251ceca7707e11a7f35e74062727
SanjayBaskaran <sanjayprakashsivakasi@gmail.com>
2023-02-16 21:19:14 +0530

    Completed with the coordinators page need to focus on quiz logic here after

--- nethunt-frontend/src/Pages/Admin/CoordinatorsAdmin.js
+++ nethunt-frontend/src/Pages/Admin/CoordinatorsAdmin.js
@@ -9,8 +9,10 @@ import HelpImg4 from "./../../Images/Help_4.svg";
 import HelpImg5 from "./../../Images/Help_5.svg";
 import HelpImg6 from "./../../Images/Help_6.svg";
 import HelpImg7 from "./../../Images/Help_7.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useAxios from "./../../utils/useAxios";
+import { serverHost } from "../../utils/server";
+
 export default function CoordinatorsAdmin(props) {
     const helpImg = [
         HelpImg1,
@@ -21,16 +23,19 @@ export default function CoordinatorsAdmin(props) {
         HelpImg6,
         HelpImg7,
     ]
-    const [loading,setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const [coordinators, setCoordinators] = useState([])
     const api = useAxios()
     useEffect(() => {
-        
-    
-      return () => {
-        
-      }
-    }, [])
-    
+        if (loading) {
+            const response = api.get("/user/get_coordinators").then(res => {
+                setCoordinators(res.data)
+                console.log(res.data)
+            })
+        }
+        setLoading(false)
+    }, [loading])
+
     return (<ThemeProvider theme={theme}>
         <CssBaseline />
         <Paper elevation={16} sx={{ padding: 2, marginTop: 3 }}>
@@ -46,63 +51,47 @@ export default function CoordinatorsAdmin(props) {
                             Our Coordinators
                         </Typography>
                         <Stack>
-                                <Grid container mt={3}>
-                                    <Grid item xs={12} padding={2}>
-                                        <Typography variant="h5">
-                                            Coordinators
-                                        </Typography>
-                                    </Grid>
-                                    <Grid item xs={12} sm={6} sx={{marginTop:{xs:2,sm:0},padding:{xs:0,sm:2}}}>
-                                        <Paper elevation={4} sx={{ padding: 2 }}>
-                                            <Stack sx={{ display: "flex", alignItems: "center", }}>
-                                                <Avatar sx={{ width: { xs: "100px", sm: "200px" }, height: { xs: "100px", sm: "200px" } }} />
-                                                <Typography>
-                                                    COORDINATOR NAME
-                                                </Typography>
-                                                <Divider />
-                                                <Box mt={2}>
-                                                    <Stack>
-                                                        <Typography variant="h6" align="center" mt={3}>
-                                                            <Container sx={{ display: "flex", alignItems: "center", gap: 2, fontSize: { xs: "15px", sm: "20px" } }}><MailRounded />
-                                                                nethuntlogin2k22@gmail.com
-                                                            </Container>
-                                                        </Typography>
-                                                        <Typography variant="h6" align="center" mt={3}>
-                                                            <Container sx={{ display: "flex", alignItems: "center", gap: 2 }}><PhoneRounded />
-                                                                +91 9677427924
-                                                            </Container>
-                                                        </Typography>
-                                                    </Stack>
-                                                </Box>
-                                            </Stack>
-                                        </Paper>
-                                    </Grid>
-                                    <Grid item xs={12} sm={6} sx={{marginTop:{xs:2,sm:0},padding:{xs:0,sm:2}}}>
-                                        <Paper elevation={4 } sx={{ padding: 2 }}>
-                                            <Stack sx={{ display: "flex", alignItems: "center", }}>
-                                                <Avatar sx={{ width: { xs: "100px", sm: "200px" }, height: { xs: "100px", sm: "200px" } }} />
-                                                <Typography>
-                                                    COORDINATOR NAME
-                                                </Typography>
-                                                <Divider />
-                                                <Box mt={2}>
-                                                    <Stack>
-                                                        <Typography variant="h6" align="center" mt={3}>
-                                                            <Container sx={{ display: "flex", alignItems: "center", gap: 2, fontSize: { xs: "15px", sm: "20px" } }}><MailRounded />
-                                                                nethuntlogin2k22@gmail.com
-                                                            </Container>
-                                                        </Typography>
-                                                        <Typography variant="h6" align="center" mt={3}>
-                                                            <Container sx={{ display: "flex", alignItems: "center", gap: 2 }}><PhoneRounded />
-                                                                +91 9677427924
-                                                            </Container>
-                                                        </Typography>
-                                                    </Stack>
-                                                </Box>
-                                            </Stack>
-                                        </Paper>
-                                    </Grid>
+                            <Grid container mt={3}>
+                                <Grid item xs={12} padding={2}>
+                                    <Typography variant="h5">
+                                        Coordinators
+                                    </Typography>
                                 </Grid>
+                                {!loading &&
+
+                                    coordinators.map(
+                                        item => {
+                                            return (<Grid item xs={12} sm={6} sx={{ marginTop: { xs: 2, sm: 0 }, padding: { xs: 0, sm: 2 } }} key={item.id}>
+                                                <Paper elevation={4} sx={{ padding: 2 }}>
+                                                    <Stack sx={{ display: "flex", alignItems: "center", }}>
+                                                        <Avatar sx={{ width: { xs: "100px", sm: "200px" }, height: { xs: "100px", sm: "200px" } }} >
+                                                            <img src={serverHost.split("/")[0]+"//"+serverHost.split("/")[2] +item.image}/>
+                                                        </Avatar>
+                                                        <Typography>
+                                                            {item.user.first_name + " " + item.user.last_name}
+                                                        </Typography>
+                                                        <Divider />
+                                                        <Box mt={2}>
+                                                            <Stack>
+                                                                <Typography variant="h6" align="center" mt={3}>
+                                                                    <Container sx={{ display: "flex", alignItems: "center", gap: 2, fontSize: { xs: "15px", sm: "20px" } }}><MailRounded />
+                                                                        {item.user.email}
+                                                                    </Container>
+                                                                </Typography>
+                                                                <Typography variant="h6" align="center" mt={3}>
+                                                                    <Container sx={{ display: "flex", alignItems: "center", gap: 2 }}><PhoneRounded />
+                                                                        +91 {item.phone}
+                                                                    </Container>
+                                                                </Typography>
+                                                            </Stack>
+                                                        </Box>
+                                                    </Stack>
+                                                </Paper>
+                                            </Grid>)
+                                        }
+                                    )
+                                }
+                            </Grid>
                         </Stack>
                     </Container>
                 </Grid>
