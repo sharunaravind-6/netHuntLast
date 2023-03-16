fd0d0eb75d76f6e3d9cb28b278a3a7992f351930
SanjayBaskaran <sanjayprakashsivakasi@gmail.com>
2023-03-16 10:11:21 +0530

    Admin home enhanced

--- nethunt-frontend/src/Components/AdminHome.js
+++ nethunt-frontend/src/Components/AdminHome.js
@@ -8,13 +8,14 @@ import { serverHost } from "../utils/server";
 import useAxios from "../utils/useAxios";
 import { theme } from "./../Theme/LightTheme";
 import Timer from "./Parts/Timer";
+import AdminSVG from "./../Images/Admin.svg"
 export default function AdminHome(props) {
     const api = useAxios()
-    const {fetchUsers,usrs} = useContext(AdminContext)
-    const [loading,setLoading] = useState(true) 
-    const [endBy,setEndBy] = useState(null)
+    const { fetchUsers, usrs } = useContext(AdminContext)
+    const [loading, setLoading] = useState(true)
+    const [endBy, setEndBy] = useState(null)
     useEffect(
-        ()=>{
+        () => {
             fetchUsers()
         },
         []
@@ -39,20 +40,31 @@ export default function AdminHome(props) {
         {endBy !== null ? <Timer timing={endBy} /> : <CircularProgress />}
         <Container maxWidth="false">
             <Grid container padding={3}>
-                <Grid item md={9}>
+                <Grid item md={9} sm={12}>
                     <Paper sx={{ padding: 3 }} elevation={10}>
-                        <Typography variant="h5" component="p">
-                            Recent Activities
-                        </Typography>
+                        <Stack sx={{ display: "flex", flexDirection: { "md": "row", "sm": "column" } ,justifyContent:"center",alignItems:"center"}}>
+                            <Box sx={{ width: { xs: "250px", sm: "450px" }, position: "relative" }}>
+                                <img src={AdminSVG} width="100%" />
+                            </Box>
+                            <Box>
+                                <Typography variant="h4" component="p" sx={{ textAlign: "center" }}>
+                                    Welcome Boss,
+                                </Typography>
+                                <Divider />
+                                <Typography  component="p" sx={{ textAlign: "justify" ,padding:2,fontSize:"18px"}}>
+                                    Welcome to your website's command center. From here, you can monitor activity, update content, and make strategic decisions that will drive your site's growth and success.
+                                </Typography>
+                            </Box>
+                        </Stack>
                         <Divider />
                     </Paper>
                 </Grid>
-                <Grid item md={3} padding={2}>
+                <Grid item md={3} sm={12} padding={2}>
                     <Paper elevation={16} sx={{ padding: 2 }}>
                         <Typography>
                             <b>Users</b>
                         </Typography>
-                        <Divider/>
+                        <Divider />
                         <List dense sx={{ width: '100%' }}>
                             {usrs.map((value) => {
                                 const labelId = `checkbox-list-secondary-label-${value}`;
@@ -60,15 +72,15 @@ export default function AdminHome(props) {
                                     <ListItem
                                         key={value.id}
                                         secondaryAction={
-                                            <CircleRounded sx={{fill: value.status === "offline" ?"red":"green",width:"10px"}}/>
+                                            <CircleRounded sx={{ fill: value.status === "offline" ? "red" : "green", width: "10px" }} />
                                         }
                                         disablePadding
                                     >
                                         <ListItemButton>
                                             <ListItemAvatar>
                                                 <Avatar>
-                                                {value.user.first_name[0]  + value.user.last_name[0]}
-                                                    </Avatar>
+                                                    {value.user.first_name[0] + value.user.last_name[0]}
+                                                </Avatar>
                                             </ListItemAvatar>
                                             <ListItemText id={labelId} primary={`${value.user.first_name + " " + value.user.last_name}`} />
                                         </ListItemButton>
