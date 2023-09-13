883064d0985bafb6e046cf0865b254dc23f5388a
M RAMANATHAN <rsai48838.com>
2023-09-14 00:58:01 +0530

    Createsuperuser Error as bean updated in the backend

--- nethuntBackend/user/models.py
+++ nethuntBackend/user/models.py
@@ -34,7 +34,7 @@ class NethuntUserManager(BaseUserManager):
 class NethuntUser(AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
     username = None
     role = models.CharField(max_length=50, choices=Role.choices)
     objects = NethuntUserManager()
